01e7f194ffec60a7ba05bf3eee1d452cb179802e
unknown <pk123456434@gmail.com>
2025-12-24 19:15:25 +0545

    Update code

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ pygame.init()
 
 screen = pygame.display.set_mode((800,600))
 
+
+#player
+
+playerImg = pygame.image.load('space-invaders.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+def player(x,y):
+    screen.blit(playerImg,(x,y))
+
+
 #game loop
 
 #title and icon
@@ -16,13 +27,35 @@ pygame.display.set_icon(icon)
 
 running = True
 while running:
+    #RGB = red green blue
+
+    screen.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-#RGB = red green blue
-    screen.fill((255,0,0))
+#if keystroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print("left is pressed")
+                playerX_change = -0.1
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                print("right is pressed")
+                playerX_change = 0.1
+
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                print("key storke has been released")
+                playerX_change = 0     
+
+    player(playerX,playerY)
+    playerX += playerX_change
     pygame.display.update()
             
 
+#movement and mechanics in game
 
+#49 min on 
